2eca1f2fe042e32adfb42922b0ccca4397d949e1
Prashant Dolnar <Prashant Dolnar@ho.rnt.ai>
2025-12-29 19:01:10 +0530

    initial commit

---
+++ src/main/java/com/router/RouterManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterManagmentSystemApplication {
+//THis is the main class
+	public static void main(String[] args) {
+		SpringApplication.run(RouterManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/router/RouterManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
